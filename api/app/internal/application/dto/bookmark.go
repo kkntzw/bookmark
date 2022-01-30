a854cac07d53adc8b107fba1ba1b2edeff48eb09
kkntzw <64656589+kkntzw@users.noreply.github.com>
2022-01-30 22:59:31 +0900

    :sparkles: ブックマークを表すDTOを追加
    アプリケーション層からプレゼンテーション層へデータを転送するためにブックマークを表すDTOを追加した。
    Refer to #6

---
+++ api/app/internal/application/dto/bookmark.go
@@ -0,0 +1,25 @@
+package dto
+
+import (
+	"github.com/kkntzw/bookmark/internal/domain/entity"
+)
+
+// ブックマークを表すDTO。
+type Bookmark struct {
+	ID   string   // ID
+	Name string   // ブックマーク名
+	URI  string   // URI
+	Tags []string // タグ一覧
+}
+
+// ブックマークを表すDTOを生成する。
+func NewBookmark(entity entity.Bookmark) Bookmark {
+	id := entity.ID()
+	name := entity.Name()
+	uri := entity.URI()
+	tags := make([]string, len(entity.Tags()))
+	for i, tag := range entity.Tags() {
+		tags[i] = tag.Value()
+	}
+	return Bookmark{id.Value(), name.Value(), uri.String(), tags}
+}

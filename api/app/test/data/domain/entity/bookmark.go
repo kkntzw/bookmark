a9fc7cefadab22b9163a005b895aa5798ea38b64
kkntzw <64656589+kkntzw@users.noreply.github.com>
2022-02-06 23:42:18 +0900

    :sparkles: テスト用にentity.Bookmark型のサンプルインスタンスを追加
    更新に関するテスト用に entity.Bookmark 型のサンプルインスタンスを追加した。
    Refer to #8

--- api/app/test/data/domain/entity/bookmark.go
+++ api/app/test/data/domain/entity/bookmark.go
@@ -17,6 +17,19 @@ func Bookmark() *entity.Bookmark {
 	return bookmark
 }
 
+// entity.Bookmark型のサンプルインスタンス。
+func ModifiedBookmark() *entity.Bookmark {
+	id, _ := entity.NewID("f81d4fae-7dec-11d0-a765-00a0c91e6bf6")
+	name, _ := entity.NewName("EXAMPLE")
+	uri, _ := entity.NewURI("http://example.com")
+	tag1, _ := entity.NewTag("1")
+	tag2, _ := entity.NewTag("2")
+	tag3, _ := entity.NewTag("3")
+	tags := []entity.Tag{*tag1, *tag2, *tag3}
+	bookmark, _ := entity.NewBookmark(id, name, uri, tags)
+	return bookmark
+}
+
 // entity.Bookmark型のサンプルインスタンスA。
 func BookmarkA() *entity.Bookmark {
 	id, _ := entity.NewID("f8ddce3a-0e87-4f3b-9f5d-148ba3125e42")

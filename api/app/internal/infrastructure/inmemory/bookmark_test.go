7012516ecb9418029b9f7f66267a8b07a74849fc
kkntzw <64656589+kkntzw@users.noreply.github.com>
2022-02-14 23:54:44 +0900

    :white_check_mark: インメモリ型ブックマークリポジトリのテストコードを修正
    ブックマークの永続化を担うインメモリ型リポジトリのテストコードをテーブル駆動テスト形式に修正した。
    Refer to #10

--- api/app/internal/infrastructure/inmemory/bookmark_test.go
+++ api/app/internal/infrastructure/inmemory/bookmark_test.go
@@ -1,127 +1,182 @@
 package inmemory
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/kkntzw/bookmark/internal/domain/entity"
 	"github.com/kkntzw/bookmark/internal/domain/repository"
-	sample_entity "github.com/kkntzw/bookmark/test/data/domain/entity"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewBookmarkRepository_repository_Bookmark型のインスタンスを返却する(t *testing.T) {
-	// when
-	object := NewBookmarkRepository()
-	// then
-	interfaceObject := (*repository.Bookmark)(nil)
-	assert.Implements(t, interfaceObject, object)
-	assert.NotNil(t, object)
-}
-
-func TestNewBookmarkRepository_戻り値は初期化済みのフィールドstoreを持つ(t *testing.T) {
-	// given
-	abstract := NewBookmarkRepository()
-	// when
-	concrete, ok := abstract.(*bookmarkRepository)
-	// then
-	assert.True(t, ok)
-	expected := map[entity.ID]entity.Bookmark{}
-	assert.Exactly(t, expected, concrete.store)
-}
-
-func TestNextID_entity_ID型のインスタンスを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	// when
-	object := repository.NextID()
-	// then
-	expectedType := &entity.ID{}
-	assert.IsType(t, expectedType, object)
-	assert.NotNil(t, object)
-}
-
-func TestSave_正当な値を受け取るとnilを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	bookmark := sample_entity.Bookmark()
-	// when
-	err := repository.Save(bookmark)
-	// then
-	assert.NoError(t, err)
-}
-
-func TestSave_不正な値を受け取るとエラーを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	bookmark := (*entity.Bookmark)(nil)
-	// when
-	err := repository.Save(bookmark)
-	// then
-	errString := "argument \"bookmark\" is nil"
-	assert.EqualError(t, err, errString)
-}
-
-func TestFindAll_ブックマークが存在する場合はentity_Bookmark型のインスタンスが含まれたスライスを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	repository.Save(sample_entity.BookmarkA())
-	repository.Save(sample_entity.BookmarkB())
-	repository.Save(sample_entity.BookmarkC())
-	// when
-	actual, err := repository.FindAll()
-	// then
-	expected := []entity.Bookmark{
-		*sample_entity.BookmarkA(),
-		*sample_entity.BookmarkB(),
-		*sample_entity.BookmarkC(),
+func ToID(t *testing.T, v string) *entity.ID {
+	t.Helper()
+	id, err := entity.NewID(v)
+	if err != nil {
+		t.Fatal(err)
 	}
-	assert.ElementsMatch(t, expected, actual)
-	assert.NoError(t, err)
+	return id
 }
 
-func TestFindAll_ブックマークが存在しない場合は空のスライスを返却する(t *testing.T) {
+func ToBookmark(t *testing.T, iv, nv, uv string, tvs ...string) *entity.Bookmark {
+	t.Helper()
+	id, err := entity.NewID(iv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	name, err := entity.NewName(nv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	uri, err := entity.NewURI(uv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tags := make([]entity.Tag, len(tvs))
+	for i, tv := range tvs {
+		tag, err := entity.NewTag(tv)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tags[i] = *tag
+	}
+	bookmark, err := entity.NewBookmark(id, name, uri, tags)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return bookmark
+}
+
+func TestNewBookmarkRepository(t *testing.T) {
+	t.Parallel()
+	{
+		t.Run("implementing bookmark repository", func(t *testing.T) {
+			t.Parallel()
+			// when
+			object := NewBookmarkRepository()
+			// then
+			assert.NotNil(t, object)
+			interfaceObject := (*repository.Bookmark)(nil)
+			assert.Implements(t, interfaceObject, object)
+		})
+	}
+	{
+		t.Run("fields", func(t *testing.T) {
+			t.Parallel()
+			// given
+			abstractRepository := NewBookmarkRepository()
+			// when
+			concreteRepository, ok := abstractRepository.(*bookmarkRepository)
+			actualStore := concreteRepository.store
+			// then
+			assert.True(t, ok)
+			expectedStore := map[entity.ID]entity.Bookmark{}
+			assert.Exactly(t, expectedStore, actualStore)
+		})
+	}
+}
+
+func TestBookmark_NextID(t *testing.T) {
+	t.Parallel()
 	// given
 	repository := NewBookmarkRepository()
 	// when
-	object, err := repository.FindAll()
+	id := repository.NextID()
 	// then
-	assert.NotNil(t, object)
-	assert.Empty(t, object)
-	assert.NoError(t, err)
+	assert.NotNil(t, id)
+	expectedType := &entity.ID{}
+	assert.IsType(t, expectedType, id)
 }
 
-func TestFindByID_該当するブックマークが存在する場合はentity_Bookmark型のインスタンスを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	repository.Save(sample_entity.Bookmark())
-	id := sample_entity.BookmarkID()
-	// when
-	actual, err := repository.FindByID(id)
-	// then
-	expected := sample_entity.Bookmark()
-	assert.Exactly(t, expected, actual)
-	assert.NoError(t, err)
+func TestBookmark_Save(t *testing.T) {
+	t.Parallel()
+	cases := map[string]struct {
+		bookmark    *entity.Bookmark
+		expectedErr error
+	}{
+		"non-nil bookmark": {ToBookmark(t, "1", "Example A", "https://foo.example.com"), nil},
+		"nil bookmark":     {nil, errors.New("argument \"bookmark\" is nil")},
+	}
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			// given
+			repository := NewBookmarkRepository()
+			// when
+			actualErr := repository.Save(tc.bookmark)
+			// then
+			assert.Exactly(t, tc.expectedErr, actualErr)
+		})
+	}
 }
 
-func TestFindByID_該当するブックマークが存在しない場合はnilを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	id := sample_entity.BookmarkID()
-	// when
-	object, err := repository.FindByID(id)
-	// then
-	assert.Nil(t, object)
-	assert.NoError(t, err)
+func TestBookmark_FindAll(t *testing.T) {
+	t.Parallel()
+	bookmark1 := ToBookmark(t, "1", "Example A", "https://foo.example.com")
+	bookmark2 := ToBookmark(t, "2", "Example B", "https://bar.example.com")
+	bookmark3 := ToBookmark(t, "3", "Example C", "https://baz.example.com")
+	cases := map[string]struct {
+		prepare           func(repository.Bookmark)
+		expectedBookmarks []entity.Bookmark
+		expectedErr       error
+	}{
+		"stored bookmarks": {
+			func(r repository.Bookmark) {
+				r.Save(bookmark1)
+				r.Save(bookmark2)
+				r.Save(bookmark3)
+			},
+			[]entity.Bookmark{*bookmark1, *bookmark2, *bookmark3},
+			nil,
+		},
+		"unstored bookmarks": {
+			func(r repository.Bookmark) {},
+			[]entity.Bookmark{},
+			nil,
+		},
+	}
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			// given
+			repository := NewBookmarkRepository()
+			tc.prepare(repository)
+			// when
+			actualBookmarks, actualErr := repository.FindAll()
+			// then
+			assert.ElementsMatch(t, tc.expectedBookmarks, actualBookmarks)
+			assert.Exactly(t, tc.expectedErr, actualErr)
+		})
+	}
 }
 
-func TestFindByID_不正な値を受け取るとエラーを返却する(t *testing.T) {
-	// given
-	repository := NewBookmarkRepository()
-	id := (*entity.ID)(nil)
-	// when
-	object, err := repository.FindByID(id)
-	// then
-	assert.Nil(t, object)
-	errString := "argument \"id\" is nil"
-	assert.EqualError(t, err, errString)
+func TestBookmark_FindByID(t *testing.T) {
+	t.Parallel()
+	bookmark := ToBookmark(t, "1", "Example A", "https://foo.example.com")
+	cases := map[string]struct {
+		prepare          func(repository.Bookmark)
+		id               *entity.ID
+		expectedBookmark *entity.Bookmark
+		expectedErr      error
+	}{
+		"stored bookmark":   {func(r repository.Bookmark) { r.Save(bookmark) }, ToID(t, "1"), bookmark, nil},
+		"unstored bookmark": {func(r repository.Bookmark) {}, ToID(t, "1"), nil, nil},
+		"nil id":            {func(r repository.Bookmark) {}, nil, nil, errors.New("argument \"id\" is nil")},
+	}
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			// given
+			repository := NewBookmarkRepository()
+			tc.prepare(repository)
+			// when
+			actualBookmark, actualErr := repository.FindByID(tc.id)
+			// then
+			assert.Exactly(t, tc.expectedBookmark, actualBookmark)
+			assert.Exactly(t, tc.expectedErr, actualErr)
+		})
+	}
 }

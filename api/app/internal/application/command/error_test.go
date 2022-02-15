063abded5c57a5644104aa5710941586b5a1cc7b
kkntzw <64656589+kkntzw@users.noreply.github.com>
2022-02-16 00:44:14 +0900

    :white_check_mark: 不正コマンドエラーのテストコードを修正
    不正コマンドを表すエラーのテストコードをテーブル駆動テスト形式に修正した。
    Refer to #10

--- api/app/internal/application/command/error_test.go
+++ api/app/internal/application/command/error_test.go
@@ -1,41 +1,45 @@
 package command
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInvalidCommandError_エラー状態を表す(t *testing.T) {
-	params := []struct {
-		err      error
-		expected string
+func TestInvalidCommandError_Error(t *testing.T) {
+	t.Parallel()
+	cases := map[string]struct {
+		args              map[string]error
+		expectedErrString string
 	}{
-		{
-			err:      &InvalidCommandError{},
-			expected: "command is invalid",
+		"nil args": {
+			nil,
+			"command is invalid",
 		},
-		{
-			err:      &InvalidCommandError{Args: map[string]error{}},
-			expected: "command is invalid",
+		"empty args": {
+			map[string]error{},
+			"command is invalid",
 		},
-		{
-			err:      &InvalidCommandError{Args: map[string]error{"A": fmt.Errorf("some error")}},
-			expected: "command is invalid: [A: some error]",
+		"one arg": {
+			map[string]error{"A": errors.New("some error")},
+			"command is invalid: [A: some error]",
 		},
-		{
-			err:      &InvalidCommandError{Args: map[string]error{"A": fmt.Errorf("some error"), "B": fmt.Errorf("some error")}},
-			expected: "command is invalid: [A: some error, B: some error]",
+		"two args": {
+			map[string]error{"A": errors.New("some error"), "B": errors.New("some error")},
+			"command is invalid: [A: some error, B: some error]",
 		},
 	}
-	for _, p := range params {
-		// given
-		err := p.err
-		// when
-		actual := err.Error()
-		// then
-		expected := p.expected
-		assert.Exactly(t, expected, actual)
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			// given
+			err := &InvalidCommandError{tc.args}
+			// when
+			actualErrString := err.Error()
+			// then
+			assert.Exactly(t, tc.expectedErrString, actualErrString)
+		})
 	}
 }

9d5392306424a3300b55bcb2c0d0d99fbc112b8c
kkntzw <64656589+kkntzw@users.noreply.github.com>
2022-02-17 23:16:37 +0900

    :white_check_mark: ブックマークDTOのテストコードを修正
    ブックマークを表すDTOのテストコードをテーブル駆動テスト形式に修正した。
    Refer to #10

--- api/app/internal/application/dto/bookmark_test.go
+++ api/app/internal/application/dto/bookmark_test.go
@@ -3,21 +3,46 @@ package dto
 import (
 	"testing"
 
-	sample_entity "github.com/kkntzw/bookmark/test/data/domain/entity"
+	"github.com/kkntzw/bookmark/internal/domain/entity"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewBookmark_Bookmark型のインスタンスを返却する(t *testing.T) {
-	// given
-	entity := sample_entity.Bookmark()
-	// when
-	actual := NewBookmark(*entity)
-	// then
-	expected := Bookmark{
-		ID:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-		Name: "example",
-		URI:  "https://example.com",
-		Tags: []string{"1", "2", "3"},
+func ToBookmark(t *testing.T, iv, nv, uv string, tvs ...string) *entity.Bookmark {
+	t.Helper()
+	id, err := entity.NewID(iv)
+	if err != nil {
+		t.Fatal(err)
 	}
-	assert.Exactly(t, expected, actual)
+	name, err := entity.NewName(nv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	uri, err := entity.NewURI(uv)
+	if err != nil {
+		t.Fatal(err)
+	}
+	tags := make([]entity.Tag, len(tvs))
+	for i, tv := range tvs {
+		tag, err := entity.NewTag(tv)
+		if err != nil {
+			t.Fatal(err)
+		}
+		tags[i] = *tag
+	}
+	bookmark, err := entity.NewBookmark(id, name, uri, tags)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return bookmark
+}
+
+func TestNewBookmark(t *testing.T) {
+	t.Parallel()
+	// given
+	entity := ToBookmark(t, "1", "Example", "https://example.com", "1-A", "1-B", "1-C")
+	// when
+	actualBookmark := NewBookmark(*entity)
+	// then
+	expectedBookmark := Bookmark{"1", "Example", "https://example.com", []string{"1-A", "1-B", "1-C"}}
+	assert.Exactly(t, expectedBookmark, actualBookmark)
 }

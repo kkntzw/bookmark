257c7c0c51788061ec4e252160ddd9ccc6c6b129
kkntzw <64656589+kkntzw@users.noreply.github.com>
2022-02-20 23:57:24 +0900

    :white_check_mark: ブックマークgRPCサーバのテストコードを修正
    ブックマークに関するgRPCサーバのテストコードをテーブル駆動テスト形式に修正した。
    Refer to #10

--- api/app/internal/presentation/server/bookmark_test.go
+++ api/app/internal/presentation/server/bookmark_test.go
@@ -2,15 +2,13 @@ package server
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/kkntzw/bookmark/internal/application/command"
 	"github.com/kkntzw/bookmark/internal/application/dto"
 	"github.com/kkntzw/bookmark/internal/presentation/pb"
-	sample_dto "github.com/kkntzw/bookmark/test/data/application/dto"
-	sample_pb "github.com/kkntzw/bookmark/test/data/presentation/pb"
 	mock_usecase "github.com/kkntzw/bookmark/test/mock/application/usecase"
 	mock_pb "github.com/kkntzw/bookmark/test/mock/presentation/pb"
 	"github.com/stretchr/testify/assert"
@@ -19,298 +17,266 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func TestNewBookmarkServer_server_Bookmark型のインスタンスを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	// when
-	object := NewBookmarkServer(usecase)
-	// then
-	interfaceObject := (*pb.BookmarkerServer)(nil)
-	assert.Implements(t, interfaceObject, object)
-	assert.NotNil(t, object)
-}
-
-func TestNewBookmarkServer_戻り値は初期化済みのフィールドusecaseを持つ(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	abstract := NewBookmarkServer(usecase)
-	// when
-	concrete, ok := abstract.(*bookmarkServer)
-	// then
-	assert.True(t, ok)
-	expectedUsecase := usecase
-	assert.Exactly(t, expectedUsecase, concrete.usecase)
-}
-
-func TestCreateBookmark_正当な値を受け取るとnilを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	cmd := &command.RegisterBookmark{
-		Name: "example",
-		URI:  "https://example.com",
-		Tags: []string{"1", "2", "3"},
+func ToCreateBookmarkRequest(t *testing.T, name, uri string, tagNames ...string) *pb.CreateBookmarkRequest {
+	t.Helper()
+	tags := make([]*pb.Tag, len(tagNames))
+	for i, tagName := range tagNames {
+		tags[i] = &pb.Tag{TagName: tagName}
 	}
-	usecase.EXPECT().Register(cmd).Return(nil)
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
 	req := &pb.CreateBookmarkRequest{
-		BookmarkName: "example",
-		Uri:          "https://example.com",
-		Tags: []*pb.Tag{
-			{TagName: "1"},
-			{TagName: "2"},
-			{TagName: "3"},
+		BookmarkName: name,
+		Uri:          uri,
+		Tags:         tags,
+	}
+	return req
+}
+
+func ToUpdateBookmarkRequest(t *testing.T, id, name, uri string) *pb.UpdateBookmarkRequest {
+	t.Helper()
+	req := &pb.UpdateBookmarkRequest{
+		BookmarkId:   id,
+		BookmarkName: name,
+		Uri:          uri,
+	}
+	return req
+}
+
+func ToBookmark(t *testing.T, id, name, uri string, tagNames ...string) *pb.Bookmark {
+	t.Helper()
+	tags := make([]*pb.Tag, len(tagNames))
+	for i, tagName := range tagNames {
+		tags[i] = &pb.Tag{TagName: tagName}
+	}
+	bookmark := &pb.Bookmark{
+		BookmarkId:   id,
+		BookmarkName: name,
+		Uri:          uri,
+		Tags:         tags,
+	}
+	return bookmark
+}
+
+func TestNewBookmarkServer(t *testing.T) {
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	{
+		t.Run("implementing bookmark server", func(t *testing.T) {
+			t.Parallel()
+			// given
+			usecase := mock_usecase.NewMockBookmark(ctrl)
+			// when
+			object := NewBookmarkServer(usecase)
+			// then
+			assert.NotNil(t, object)
+			interfaceObject := (*pb.BookmarkerServer)(nil)
+			assert.Implements(t, interfaceObject, object)
+		})
+	}
+	{
+		t.Run("fields", func(t *testing.T) {
+			t.Parallel()
+			// given
+			usecase := mock_usecase.NewMockBookmark(ctrl)
+			abstractServer := NewBookmarkServer(usecase)
+			// when
+			concreteServer, ok := abstractServer.(*bookmarkServer)
+			actualUsecase := concreteServer.usecase
+			// then
+			assert.True(t, ok)
+			expectedUsecase := usecase
+			assert.Exactly(t, expectedUsecase, actualUsecase)
+		})
+	}
+}
+
+func TestBookmark_CreateBookmark(t *testing.T) {
+	t.Parallel()
+	cmd := &command.RegisterBookmark{Name: "Example A", URI: "https://foo.example.com", Tags: []string{"1-A", "1-B", "1-C"}}
+	req := ToCreateBookmarkRequest(t, "Example A", "https://foo.example.com", "1-A", "1-B", "1-C")
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	cases := map[string]struct {
+		prepare          func(*mock_usecase.MockBookmark)
+		req              *pb.CreateBookmarkRequest
+		expectedResponse *emptypb.Empty
+		expectedErr      error
+	}{
+		"non-nil request": {
+			func(usecase *mock_usecase.MockBookmark) {
+				usecase.EXPECT().Register(cmd).Return(nil)
+			},
+			req,
+			&emptypb.Empty{},
+			nil,
+		},
+		"nil request": {
+			func(usecase *mock_usecase.MockBookmark) {},
+			nil,
+			nil,
+			status.Error(codes.InvalidArgument, "argument \"req\" is nil"),
+		},
+		"invalid request": {
+			func(usecase *mock_usecase.MockBookmark) {
+				usecase.
+					EXPECT().
+					Register(&command.RegisterBookmark{Name: "Example A", URI: "", Tags: []string{}}).
+					Return(&command.InvalidCommandError{Args: map[string]error{"URI": errors.New("some error")}})
+			},
+			ToCreateBookmarkRequest(t, "Example A", ""),
+			nil,
+			status.Error(codes.InvalidArgument, "request is invalid"),
+		},
+		"failed at usecase.Register": {
+			func(usecase *mock_usecase.MockBookmark) {
+				usecase.EXPECT().Register(cmd).Return(errors.New("some error"))
+			},
+			req,
+			nil,
+			status.Error(codes.Internal, "server error"),
 		},
 	}
-	// when
-	actual, err := server.CreateBookmark(ctx, req)
-	// then
-	expected := &emptypb.Empty{}
-	assert.Exactly(t, expected, actual)
-	assert.NoError(t, err)
-}
-
-func TestCreateBookmark_不正な値を受け取るとInvalidArgumentを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := (*pb.CreateBookmarkRequest)(nil)
-	// when
-	object, actual := server.CreateBookmark(ctx, req)
-	// then
-	assert.Nil(t, object)
-	expected := status.Error(codes.InvalidArgument, "argument \"req\" is nil")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestCreateBookmark_コマンドが不正な場合はInvalidArgumentを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	cmd := &command.RegisterBookmark{
-		Name: "",
-		URI:  "",
-		Tags: []string{""},
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			usecase := mock_usecase.NewMockBookmark(ctrl)
+			tc.prepare(usecase)
+			// given
+			server := NewBookmarkServer(usecase)
+			ctx := context.TODO()
+			// when
+			actualResponse, actualErr := server.CreateBookmark(ctx, tc.req)
+			// then
+			assert.Exactly(t, tc.expectedResponse, actualResponse)
+			assert.Exactly(t, tc.expectedErr, actualErr)
+		})
 	}
-	usecase.EXPECT().Register(cmd).Return(&command.InvalidCommandError{})
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := &pb.CreateBookmarkRequest{BookmarkName: "", Uri: "", Tags: []*pb.Tag{{TagName: ""}}}
-	// when
-	object, actual := server.CreateBookmark(ctx, req)
-	// then
-	assert.Nil(t, object)
-	expected := status.Error(codes.InvalidArgument, "request is invalid")
-	assert.Exactly(t, expected, actual)
 }
 
-func TestCreateBookmark_ブックマーク登録中にエラーが発生した場合はInternalを返却する(t *testing.T) {
+func TestBookmark_ListBookmarks(t *testing.T) {
+	t.Parallel()
+	bookmarks := []dto.Bookmark{
+		{ID: "1", Name: "Example A", URI: "https://foo.example.com", Tags: []string{}},
+		{ID: "2", Name: "Example B", URI: "https://bar.example.com", Tags: []string{"2-A"}},
+		{ID: "3", Name: "Example C", URI: "https://baz.example.com", Tags: []string{"3-A", "3-B"}},
+	}
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	cmd := &command.RegisterBookmark{
-		Name: "example",
-		URI:  "https://example.com",
-		Tags: []string{"1", "2", "3"},
-	}
-	usecase.EXPECT().Register(cmd).Return(fmt.Errorf("some error"))
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := &pb.CreateBookmarkRequest{
-		BookmarkName: "example",
-		Uri:          "https://example.com",
-		Tags: []*pb.Tag{
-			{TagName: "1"},
-			{TagName: "2"},
-			{TagName: "3"},
+	cases := map[string]struct {
+		prepare     func(*mock_usecase.MockBookmark, *mock_pb.MockBookmarker_ListBookmarksServer)
+		req         *emptypb.Empty
+		expectedErr error
+	}{
+		"non-nil request": {
+			func(usecase *mock_usecase.MockBookmark, stream *mock_pb.MockBookmarker_ListBookmarksServer) {
+				usecase.EXPECT().List().Return(bookmarks, nil)
+				stream.EXPECT().Send(ToBookmark(t, "1", "Example A", "https://foo.example.com")).Return(nil)
+				stream.EXPECT().Send(ToBookmark(t, "2", "Example B", "https://bar.example.com", "2-A")).Return(nil)
+				stream.EXPECT().Send(ToBookmark(t, "3", "Example C", "https://baz.example.com", "3-A", "3-B")).Return(nil)
+			},
+			&emptypb.Empty{},
+			nil,
+		},
+		"nil request": {
+			func(usecase *mock_usecase.MockBookmark, stream *mock_pb.MockBookmarker_ListBookmarksServer) {},
+			nil,
+			status.Error(codes.InvalidArgument, "argument \"req\" is nil"),
+		},
+		"failed at usecase.List": {
+			func(usecase *mock_usecase.MockBookmark, stream *mock_pb.MockBookmarker_ListBookmarksServer) {
+				usecase.EXPECT().List().Return(nil, errors.New("some error"))
+			},
+			&emptypb.Empty{},
+			status.Error(codes.Internal, "server error"),
+		},
+		"failed at stream.Send": {
+			func(usecase *mock_usecase.MockBookmark, stream *mock_pb.MockBookmarker_ListBookmarksServer) {
+				usecase.EXPECT().List().Return(bookmarks, nil)
+				stream.EXPECT().Send(ToBookmark(t, "1", "Example A", "https://foo.example.com")).Return(nil)
+				stream.EXPECT().Send(ToBookmark(t, "2", "Example B", "https://bar.example.com", "2-A")).Return(errors.New("some error"))
+			},
+			&emptypb.Empty{},
+			status.Error(codes.Internal, "response failed"),
 		},
 	}
-	// when
-	object, actual := server.CreateBookmark(ctx, req)
-	// then
-	assert.Nil(t, object)
-	expected := status.Error(codes.Internal, "server error")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestListBookmarks_正当な値を受け取るとnilを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	bookmarks := []dto.Bookmark{
-		sample_dto.BookmarkA(),
-		sample_dto.BookmarkB(),
-		sample_dto.BookmarkC(),
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			usecase := mock_usecase.NewMockBookmark(ctrl)
+			stream := mock_pb.NewMockBookmarker_ListBookmarksServer(ctrl)
+			tc.prepare(usecase, stream)
+			// given
+			server := NewBookmarkServer(usecase)
+			// when
+			actualErr := server.ListBookmarks(tc.req, stream)
+			// then
+			assert.Exactly(t, tc.expectedErr, actualErr)
+		})
 	}
-	usecase.EXPECT().List().Return(bookmarks, nil)
-	server := NewBookmarkServer(usecase)
-	req := &emptypb.Empty{}
-	stream := mock_pb.NewMockBookmarker_ListBookmarksServer(ctrl)
-	stream.EXPECT().Send(sample_pb.BookmarkA()).Return(nil)
-	stream.EXPECT().Send(sample_pb.BookmarkB()).Return(nil)
-	stream.EXPECT().Send(sample_pb.BookmarkC()).Return(nil)
-	// when
-	err := server.ListBookmarks(req, stream)
-	// then
-	assert.NoError(t, err)
 }
 
-func TestListBookmarks_不正な値を受け取るとInvalidArgumentを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	server := NewBookmarkServer(usecase)
-	req := (*emptypb.Empty)(nil)
-	stream := mock_pb.NewMockBookmarker_ListBookmarksServer(ctrl)
-	// when
-	actual := server.ListBookmarks(req, stream)
-	// then
-	expected := status.Error(codes.InvalidArgument, "argument \"req\" is nil")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestListBookmarks_ブックマーク一覧取得中にエラーが発生した場合はInternalを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	usecase.EXPECT().List().Return(nil, fmt.Errorf("some error"))
-	server := NewBookmarkServer(usecase)
-	req := &emptypb.Empty{}
-	stream := mock_pb.NewMockBookmarker_ListBookmarksServer(ctrl)
-	// when
-	actual := server.ListBookmarks(req, stream)
-	// then
-	expected := status.Error(codes.Internal, "server error")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestListBookmarks_ストリーム送信中にエラーが発生した場合はInternalを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	bookmarks := []dto.Bookmark{
-		sample_dto.BookmarkA(),
-		sample_dto.BookmarkB(),
-		sample_dto.BookmarkC(),
-	}
-	usecase.EXPECT().List().Return(bookmarks, nil)
-	server := NewBookmarkServer(usecase)
-	req := &emptypb.Empty{}
-	stream := mock_pb.NewMockBookmarker_ListBookmarksServer(ctrl)
-	stream.EXPECT().Send(sample_pb.BookmarkA()).Return(nil)
-	stream.EXPECT().Send(sample_pb.BookmarkB()).Return(fmt.Errorf("some error"))
-	// when
-	actual := server.ListBookmarks(req, stream)
-	// then
-	expected := status.Error(codes.Internal, "response failed")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestUpdateBookmark_正当な値を受け取るとnilを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
+func TestBookmark_UpdateBookmark(t *testing.T) {
+	t.Parallel()
 	cmd := &command.UpdateBookmark{
-		ID:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-		Name: "EXAMPLE",
-		URI:  "http://example.com",
+		ID:   "1",
+		Name: "Example Bar",
+		URI:  "https://foo.example.com/bar",
 	}
-	usecase.EXPECT().Update(cmd).Return(nil)
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := &pb.UpdateBookmarkRequest{
-		BookmarkId:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-		BookmarkName: "EXAMPLE",
-		Uri:          "http://example.com",
-	}
-	// when
-	actual, err := server.UpdateBookmark(ctx, req)
-	// then
-	expected := &emptypb.Empty{}
-	assert.Exactly(t, expected, actual)
-	assert.NoError(t, err)
-}
-
-func TestUpdateBookmark_不正な値を受け取るとInvalidArgumentを返却する(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := (*pb.UpdateBookmarkRequest)(nil)
-	// when
-	object, actual := server.UpdateBookmark(ctx, req)
-	// then
-	assert.Nil(t, object)
-	expected := status.Error(codes.InvalidArgument, "argument \"req\" is nil")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestUpdateBookmark_コマンドが不正な場合はInvalidArgumentを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	cmd := &command.UpdateBookmark{
-		ID:   "",
-		Name: "",
-		URI:  "",
+	cases := map[string]struct {
+		prepare          func(*mock_usecase.MockBookmark)
+		req              *pb.UpdateBookmarkRequest
+		expectedResponse *emptypb.Empty
+		expectedErr      error
+	}{
+		"non-nil request": {
+			func(usecase *mock_usecase.MockBookmark) {
+				usecase.EXPECT().Update(cmd).Return(nil)
+			},
+			ToUpdateBookmarkRequest(t, "1", "Example Bar", "https://foo.example.com/bar"),
+			&emptypb.Empty{},
+			nil,
+		},
+		"nil request": {
+			func(usecase *mock_usecase.MockBookmark) {},
+			nil,
+			nil,
+			status.Error(codes.InvalidArgument, "argument \"req\" is nil"),
+		},
+		"invalid request": {
+			func(usecase *mock_usecase.MockBookmark) {
+				usecase.EXPECT().Update(&command.UpdateBookmark{ID: "", Name: "", URI: ""}).Return(&command.InvalidCommandError{})
+			},
+			ToUpdateBookmarkRequest(t, "", "", ""),
+			nil,
+			status.Error(codes.InvalidArgument, "request is invalid"),
+		},
+		"failed at usecase.Update": {
+			func(usecase *mock_usecase.MockBookmark) {
+				usecase.EXPECT().Update(cmd).Return(errors.New("some error"))
+			},
+			ToUpdateBookmarkRequest(t, "1", "Example Bar", "https://foo.example.com/bar"),
+			nil,
+			status.Error(codes.Internal, "server error"),
+		},
 	}
-	usecase.EXPECT().Update(cmd).Return(&command.InvalidCommandError{})
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := &pb.UpdateBookmarkRequest{
-		BookmarkId:   "",
-		BookmarkName: "",
-		Uri:          "",
+	for name, tc := range cases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			usecase := mock_usecase.NewMockBookmark(ctrl)
+			tc.prepare(usecase)
+			// given
+			server := NewBookmarkServer(usecase)
+			ctx := context.TODO()
+			// when
+			actualResponse, actualErr := server.UpdateBookmark(ctx, tc.req)
+			// then
+			assert.Exactly(t, tc.expectedResponse, actualResponse)
+			assert.Exactly(t, tc.expectedErr, actualErr)
+		})
 	}
-	// when
-	object, actual := server.UpdateBookmark(ctx, req)
-	// then
-	assert.Nil(t, object)
-	expected := status.Error(codes.InvalidArgument, "request is invalid")
-	assert.Exactly(t, expected, actual)
-}
-
-func TestUpdateBookmark_ブックマークの更新に失敗した場合はInternalを返却する(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	// given
-	usecase := mock_usecase.NewMockBookmark(ctrl)
-	cmd := &command.UpdateBookmark{
-		ID:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-		Name: "EXAMPLE",
-		URI:  "http://example.com",
-	}
-	usecase.EXPECT().Update(cmd).Return(fmt.Errorf("some error"))
-	server := NewBookmarkServer(usecase)
-	ctx := context.TODO()
-	req := &pb.UpdateBookmarkRequest{
-		BookmarkId:   "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
-		BookmarkName: "EXAMPLE",
-		Uri:          "http://example.com",
-	}
-	// when
-	object, actual := server.UpdateBookmark(ctx, req)
-	// then
-	assert.Nil(t, object)
-	expected := status.Error(codes.Internal, "server error")
-	assert.Exactly(t, expected, actual)
 }
